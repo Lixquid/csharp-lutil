8a22af973c97849549055940df2629ee13b713a7
Lixquid <Lixquid@users.noreply.github.com>
2018-06-15 14:45:03 +0100

    Added RNGRandom

---
+++ LUtil.Test/Random/RNGRandomTests.cs
@@ -0,0 +1,46 @@
+﻿using System.Security.Cryptography;
+using LUtil.Random;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LUtil.Test.Random {
+
+
+    [TestClass]
+    public class RNGRandomTests {
+
+        private class MockRNG : RandomNumberGenerator {
+            private byte Counter { get; set; } = 0;
+
+            private byte GetByte() => Counter++;
+
+            public override void GetBytes(byte[] data) {
+                for (var i = 0; i < data.Length; i++) {
+                    data[i] = GetByte();
+                }
+            }
+        }
+
+        [TestMethod]
+        public void RandomOverriddenMethods() {
+            var r = new RNGRandom(new MockRNG());
+
+            double d;
+            int i;
+            for (var x = 0; x < 50000; x++) {
+                d = r.NextDouble();
+                Assert.IsTrue(0 <= d);
+                Assert.IsTrue(d < 1);
+                i = r.Next();
+                Assert.IsTrue(0 <= i);
+                Assert.IsTrue(i < int.MaxValue);
+                i = r.Next(10);
+                Assert.IsTrue(0 <= i);
+                Assert.IsTrue(i < 10);
+                i = r.Next(-10, 10);
+                Assert.IsTrue(-10 <= i);
+                Assert.IsTrue(i < 10);
+            }
+        }
+
+    }
+}
---
+++ LUtil/Random/RNGRandom.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace LUtil.Random {
+    public class RNGRandom : System.Random {
+        private RandomNumberGenerator Source { get; }
+
+        /// <summary>
+        ///     Creates a new RNGRandom using the specified
+        ///     <see cref="RandomNumberGenerator"/> source.
+        /// </summary>
+        /// <param name="source">The Random source to use.</param>
+        public RNGRandom(RandomNumberGenerator source) {
+            Source = source;
+        }
+
+        /// <inheritdoc cref="System.Random.Sample"/>
+        protected override double Sample() {
+            var buffer = new byte[8];
+            Source.GetBytes(buffer);
+            // Grab the 53 bits of integer precision (max value, 2^53 - 1),
+            // divide by 2^53.
+            // Ensures 0 <= ( 2^53 - 1 ) / 2^53 < 1
+            return (double)(BitConverter.ToUInt64(buffer, 0) >> 11) / (1UL << 53);
+        }
+
+        /// <inheritdoc cref="System.Random.NextDouble"/>
+        public override double NextDouble() => Sample();
+        /// <inheritdoc cref="System.Random.NextBytes"/>
+        public override void NextBytes(byte[] buffer) => Source.GetBytes(buffer);
+        /// <inheritdoc cref="System.Random.Next()"/>
+        public override int Next() => (int)(Sample() * int.MaxValue);
+        /// <inheritdoc cref="System.Random.Next(int)"/>
+        public override int Next(int maxValue) => (int)(Sample() * maxValue);
+        /// <inheritdoc cref="System.Random.Next(int,int)"/>
+        public override int Next(int minValue, int maxValue) =>
+            (int)(Sample() * (maxValue - minValue) + minValue);
+
+    }
+}
