ae9ad447d52a6dc3feb653d90af0ffc0ee440ab3
Lixquid <Lixquid@users.noreply.github.com>
2018-09-17 10:29:14 +0100

    Added ComparableHelper.IsBetween

---
+++ LUtil.Test/Helpers/ComparableHelperTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using LUtil.Helpers.Extensions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using static LUtil.Helpers.ComparableHelper;
+
+namespace LUtil.Test.Helpers {
+    [TestClass]
+    public class ComparableHelperTests {
+        [TestMethod]
+        public void IsBetween_Inclusivity() {
+            // Base
+            Assert.IsTrue(5.IsBetween(0, 10));
+            Assert.IsFalse((-5).IsBetween(0, 10));
+            Assert.IsFalse(15.IsBetween(0, 10));
+            // Inclusivity
+            Assert.IsTrue(0.IsBetween(0, 10, IsBetweenInclusivity.Inclusive));
+            Assert.IsFalse(0.IsBetween(0, 10, IsBetweenInclusivity.Exclusive));
+            Assert.IsTrue(0.IsBetween(0, 10, IsBetweenInclusivity.MinInclusiveMaxExclusive));
+            Assert.IsFalse(0.IsBetween(0, 10, IsBetweenInclusivity.MinExclusiveMaxInclusive));
+
+            Assert.IsTrue(5.IsBetween(0, 10, IsBetweenInclusivity.Inclusive));
+            Assert.IsTrue(5.IsBetween(0, 10, IsBetweenInclusivity.Exclusive));
+            Assert.IsTrue(5.IsBetween(0, 10, IsBetweenInclusivity.MinExclusiveMaxInclusive));
+            Assert.IsTrue(5.IsBetween(0, 10, IsBetweenInclusivity.MinInclusiveMaxExclusive));
+
+            Assert.IsTrue(10.IsBetween(0, 10, IsBetweenInclusivity.Inclusive));
+            Assert.IsFalse(10.IsBetween(0, 10, IsBetweenInclusivity.Exclusive));
+            Assert.IsTrue(10.IsBetween(0, 10, IsBetweenInclusivity.MinExclusiveMaxInclusive));
+            Assert.IsFalse(10.IsBetween(0, 10, IsBetweenInclusivity.MinInclusiveMaxExclusive));
+
+            Assert.IsFalse((-5).IsBetween(0, 10, IsBetweenInclusivity.Inclusive));
+            Assert.IsFalse((-5).IsBetween(0, 10, IsBetweenInclusivity.Exclusive));
+            Assert.IsFalse((-5).IsBetween(0, 10, IsBetweenInclusivity.MinExclusiveMaxInclusive));
+            Assert.IsFalse((-5).IsBetween(0, 10, IsBetweenInclusivity.MinInclusiveMaxExclusive));
+
+            Assert.IsFalse(15.IsBetween(0, 10, IsBetweenInclusivity.Inclusive));
+            Assert.IsFalse(15.IsBetween(0, 10, IsBetweenInclusivity.Exclusive));
+            Assert.IsFalse(15.IsBetween(0, 10, IsBetweenInclusivity.MinExclusiveMaxInclusive));
+            Assert.IsFalse(15.IsBetween(0, 10, IsBetweenInclusivity.MinInclusiveMaxExclusive));
+        }
+
+        [TestMethod]
+        public void IsBetween_NonNumeric() {
+            Assert.IsTrue(new DateTime(2000, 1, 1).IsBetween(new DateTime(1999, 12, 1), new DateTime(2000, 2, 1)));
+            Assert.IsFalse(new DateTime(2000, 1, 1).IsBetween(new DateTime(2010, 1, 1), new DateTime(2011, 1, 1)));
+        }
+    }
+}
---
+++ LUtil/Helpers/Extensions/ComparableHelperExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace LUtil.Helpers.Extensions {
+    public static class ComparableHelperExtensions {
+        /// <inheritdoc cref="ComparableHelper.IsBetween{T}"/>
+        public static bool IsBetween<T>(
+            this T value,
+            T min,
+            T max,
+            ComparableHelper.IsBetweenInclusivity inclusivity = ComparableHelper.IsBetweenInclusivity.Inclusive
+        ) where T : IComparable<T> =>
+            ComparableHelper.IsBetween(value, min, max, inclusivity);
+    }
+}
