7ba6f70937c47bb928587a5a5575a6845d6e90bc
Lixquid <Lixquid@users.noreply.github.com>
2018-09-17 09:38:15 +0100

    Added RandomHelper.NextString

---
+++ LUtil/Helpers/Extensions/RandomHelperExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace LUtil.Helpers.Extensions {
+    public static class RandomHelperExtensions {
+        /// <inheritdoc cref="RandomHelper.NextString(System.Random,int,System.Collections.Generic.IList{char})"/>
+        public static string NextString(this System.Random random, int length, IList<char> source) =>
+            RandomHelper.NextString(random, length, source);
+
+        /// <inheritdoc cref="NextString(System.Random,int,string)"/>
+        public static string NextString(
+            System.Random random,
+            int length,
+            string source = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        ) =>
+            RandomHelper.NextString(random, length, source);
+    }
+}
---
+++ LUtil/Helpers/RandomHelper.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace LUtil.Helpers {
+    public static class RandomHelper {
+        /// <summary>
+        ///     Generates a new random string of the specified length, composed
+        ///     of characters from the specified source.
+        /// </summary>
+        /// <param name="random">The source of randomness.</param>
+        /// <param name="length">The length of string to generate.</param>
+        /// <param name="source">
+        ///     The source of characters to build the string from.
+        ///
+        ///     If a character appears multiple times in the source, it is more
+        ///     likely to appear in the output string.
+        /// </param>
+        public static string NextString(System.Random random, int length, IList<char> source) {
+            var output = new char[length];
+            for (var i = 0; i < length; i++) {
+                output[i] = source[random.Next(source.Count)];
+            }
+
+            return string.Concat(output);
+        }
+
+        /// <inheritdoc cref="NextString(System.Random,int,System.Collections.Generic.IList{char})"/>
+        public static string NextString(
+            System.Random random,
+            int length,
+            string source = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        ) =>
+            NextString(random, length, source.ToCharArray());
+    }
+}
