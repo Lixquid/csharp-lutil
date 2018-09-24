f888de3cfedeafa1235bb012f94ac330a8b0c79c
Lixquid <Lixquid@users.noreply.github.com>
2018-09-24 13:42:51 +0100

    Added EnumerableHelper.Random

---
+++ LUtil/Helpers/EnumerableHelper.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+
+namespace LUtil.Helpers {
+    public static class EnumerableHelper {
+        /// <summary>
+        ///     Returns a random value from an enumerable.
+        ///
+        ///     Warning: This will enumerate the enumerable!
+        /// </summary>
+        /// <typeparam name="T">The type enumerated in the enumerable.</typeparam>
+        /// <param name="enumerable">The enumerable to get a random value from.</param>
+        /// <param name="random">The source of randomness.</param>
+        public static T Random<T>(IEnumerable<T> enumerable, System.Random random) =>
+            RandomHelper.NextFromEnumerable(random, enumerable);
+
+        private static readonly System.Random _randomDefault = new System.Random();
+        private static readonly object _randomDefaultLock = new object();
+
+
+        /// <summary>
+        ///     Returns a random value from an enumerable.
+        ///
+        ///     Warning: This will enumerate the enumerable!
+        /// </summary>
+        /// <typeparam name="T">The type enumerated in the enumerable.</typeparam>
+        /// <param name="enumerable">The enumerable to get a random value from.</param>
+        /// <remarks>
+        ///     Behind the scenes, this uses a <c>static</c>, <c>lock</c>ed
+        ///     <see cref="System.Random"/> to generate random values. To use
+        ///     your own source of randomness, see
+        ///     <see cref="Random{T}(IEnumerable{T},System.Random)"/>.
+        /// </remarks>
+        public static T Random<T>(IEnumerable<T> enumerable) {
+            lock (_randomDefaultLock) {
+                return Random(enumerable, _randomDefault);
+            }
+        }
+
+    }
+}
---
+++ LUtil/Helpers/Extensions/EnumerableHelperExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace LUtil.Helpers.Extensions {
+    public static class EnumerableHelperExtensions {
+        /// <inheritdoc cref="EnumerableHelper.Random{T}(IEnumerable{T},System.Random)"/>
+        public static T Random<T>(this IEnumerable<T> enumerable, System.Random random) =>
+            EnumerableHelper.Random(enumerable, random);
+
+        /// <inheritdoc cref="EnumerableHelper.Random{T}(IEnumerable{T})"/>
+        public static T Random<T>(this IEnumerable<T> enumerable) =>
+            EnumerableHelper.Random(enumerable);
+    }
+}
