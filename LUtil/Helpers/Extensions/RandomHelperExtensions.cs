081ee1169a5240ec43b2b82bd33ad15a4b1f30df
Lixquid <Lixquid@users.noreply.github.com>
2018-09-24 10:06:06 +0100

    Fixed RandomHelperExtensions

--- LUtil/Helpers/Extensions/RandomHelperExtensions.cs
+++ LUtil/Helpers/Extensions/RandomHelperExtensions.cs
@@ -6,9 +6,9 @@ namespace LUtil.Helpers.Extensions {
         public static string NextString(this System.Random random, int length, IList<char> source) =>
             RandomHelper.NextString(random, length, source);
 
-        /// <inheritdoc cref="NextString(System.Random,int,string)"/>
+        /// <inheritdoc cref="RandomHelper.NextString(System.Random,int,string)"/>
         public static string NextString(
-            System.Random random,
+            this System.Random random,
             int length,
             string source = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
         ) =>
