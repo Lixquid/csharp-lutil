39b5cbc5a32513801d804554ea3ab7c7c664a7ac
Lixquid <Lixquid@users.noreply.github.com>
2018-11-15 21:05:40 +0000

    Added StaticRandom

---
+++ LUtil/Random/StaticRandom.cs
@@ -0,0 +1,43 @@
+﻿using JetBrains.Annotations;
+
+namespace LUtil.Random {
+    /// <summary>
+    ///     Contains thread-safe, static methods to return values from the
+    ///     default <see cref="System.Random"/> class.
+    /// </summary>
+    [PublicAPI]
+    public static class StaticRandom {
+        private static readonly System.Random Random = new System.Random();
+        private static readonly object RandomLock = new object();
+
+        /// <inheritdoc cref="System.Random.Next()"/>
+        public static int Next() {
+            lock (RandomLock)
+                return Random.Next();
+        }
+
+        /// <inheritdoc cref="System.Random.Next(int)"/>
+        public static int Next(int maxValue) {
+            lock (RandomLock)
+                return Random.Next(maxValue);
+        }
+
+        /// <inheritdoc cref="System.Random.Next(int,int)"/>
+        public static int Next(int minValue, int maxValue) {
+            lock (RandomLock)
+                return Random.Next(minValue, maxValue);
+        }
+
+        /// <inheritdoc cref="System.Random.NextBytes"/>
+        public static void NextBytes(byte[] buffer) {
+            lock (RandomLock)
+                Random.NextBytes(buffer);
+        }
+
+        /// <inheritdoc cref="System.Random.NextDouble"/>
+        public static double NextDouble() {
+            lock (RandomLock)
+                return Random.NextDouble();
+        }
+    }
+}
