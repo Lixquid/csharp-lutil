0922835a8c919c6df6c551b4a86138931e1eb2f8
Lixquid <Lixquid@users.noreply.github.com>
2018-11-19 15:25:36 +0000

    Added TypeHelper

---
+++ LUtil.Test/Helpers/TypeHelperTests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using LUtil.Helpers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LUtil.Test.Helpers {
+    [TestClass]
+    public class TypeHelperTests {
+        [TestMethod]
+        public void GetAllImplementedTypes() {
+            Assert.IsTrue(new HashSet<Type> {
+                typeof(IComparable),
+                typeof(IFormattable),
+                typeof(IConvertible),
+                typeof(IComparable<int>),
+                typeof(IEquatable<int>),
+                typeof(ValueType),
+                typeof(object)
+            }.SetEquals(TypeHelper.GetAllImplementedTypes(typeof(int))));
+
+            Assert.IsTrue(new HashSet<Type> {
+                typeof(IEnumerable)
+            }.SetEquals(TypeHelper.GetAllImplementedTypes(typeof(IEnumerable<int>))));
+
+            Assert.IsTrue(new HashSet<Type> {
+                typeof(IEnumerable),
+                // The T in IEnumerable<T> is *technically* more unbound than
+                // the T in ICollection<T>, even though they accept the same
+                // type. So, we need to construct a "more-bound" unbound type.
+                // You can get the fully unbound IEnumerable<T> type by adding
+                // withUnboundGenerics
+                typeof(IEnumerable<>).MakeGenericType(typeof(ICollection<>).GetGenericArguments())
+            }.SetEquals(TypeHelper.GetAllImplementedTypes(typeof(ICollection<>))));
+
+            Assert.IsTrue(new HashSet<Type> {
+                typeof(IComparable),
+                typeof(IFormattable),
+                typeof(IConvertible),
+                typeof(IComparable<int>),
+                typeof(IEquatable<int>),
+                typeof(ValueType),
+                typeof(object),
+                typeof(IComparable<>),
+                typeof(IEquatable<>)
+            }.SetEquals(TypeHelper.GetAllImplementedTypes(typeof(int), withUnboundGenerics: true)));
+        }
+    }
+}
---
+++ LUtil/Helpers/Extensions/TypeHelperExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+
+namespace LUtil.Helpers.Extensions {
+    [PublicAPI]
+    public static class TypeHelperExtensions {
+        /// <inheritdoc cref="TypeHelper.GetAllImplementedTypes"/>
+        public static ISet<Type> GetAllImplementedTypes([NotNull] this Type type, bool withUnboundGenerics = false) =>
+            TypeHelper.GetAllImplementedTypes(type);
+    }
+}
