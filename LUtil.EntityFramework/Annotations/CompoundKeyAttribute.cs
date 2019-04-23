301bc3be9c2d01afce6f6b7c3584c75fc9c5fe25
Lixquid <lixquid@waifu.club>
2019-04-23 16:58:25 +0100

    Added missing EntityFramework.CompoundKey attribute documentation

--- LUtil.EntityFramework/Annotations/CompoundKeyAttribute.cs
+++ LUtil.EntityFramework/Annotations/CompoundKeyAttribute.cs
@@ -2,6 +2,10 @@ using System;
 using JetBrains.Annotations;
 
 namespace LUtil.EntityFramework.Annotations {
+    /// <summary>
+    ///     Adds a Primary Key to an entity comprised of all properties with
+    ///     this attribute.
+    /// </summary>
     [PublicAPI, AttributeUsage(AttributeTargets.Property)]
     public class CompoundKeyAttribute : Attribute {
         public int Order { get; }
